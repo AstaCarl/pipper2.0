598d18f5eb30c0d81f9379b7417bc473ee02cc06
Julie <jc.lauesen@gmail.com>
2023-09-21 11:56:28 +0200

    update

--- src/components/opretpip.jsx
+++ src/components/opretpip.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import avatar from './adventurer-1695289491040.png'
 
 export default function Opretpip(props) {
     return(
         <body>
-            <p>hej med dig</p>
+
+            <h1>Home</h1>
+            
+            <avatar>
+                <img className='logo' src={avatar} alt="avatar" />
+                <p>Hvad har du på hjertet?</p>
+            </avatar>
+
 
         </body>
     )
