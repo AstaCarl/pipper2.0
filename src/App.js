677573ed9c4f428ad99eb3cc4a932feb5d9254a1
Julie <jc.lauesen@gmail.com>
2023-09-21 11:15:25 +0200

    components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import "./components/sidebar.css";
 import Sidebar from "./components/Sidebar";
 
+import "./components/opretpip.css";
+import Opretpip from "./components/Opretpip";
+
 function App() {
   return (
     <div className="App">
       <Sidebar/>
+      <Opretpip/>
     </div>
   );
 }
---
+++ src/components/opretpip.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+export default function Opretpip(props) {
+    return(
+        <body>
+            <p>hej med dig</p>
+
+        </body>
+    )
+}
