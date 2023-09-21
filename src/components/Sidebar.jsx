4b32374569259bb3bfce1a6e44e92b30375c4ef9
Asta Carl <astamig@hotmail.com>
2023-09-21 11:41:20 +0200

    added logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import myLogo from './twitter.logo.png'
 
 export default function Sidebar(props) {
     return (
         <body>  
             <nav class="nav flex-column sidebar">
+                <figure>
+                <img className='logo' src={myLogo} alt="Logo" />
+                <h1>PIPPER</h1>
+                </figure>
                 <a class="nav-link active" aria-current="page" href="#">Active</a>
                 <a class="nav-link" href="#">Link</a>
                 <a class="nav-link" href="#">Link</a>
