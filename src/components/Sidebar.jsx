df92246738ac5c349418d906367b806f6e4e4d29
Asta Carl <astamig@hotmail.com>
2023-09-21 12:21:01 +0200

    added stuff

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import myLogo from './twitter.logo.png'
+import myAvatar from './adventurer-1695289491040.png'
 
 export default function Sidebar(props) {
     return (
@@ -11,9 +12,14 @@ export default function Sidebar(props) {
                 <img className='logo' src={myLogo} alt="Logo" />
                 <h1>PIPPER</h1>
                 </figure>
-                <a class="nav-link active" aria-current="page" href="#">Active</a>
-                <a class="nav-link" href="#">Link</a>
-                <a class="nav-link" href="#">Link</a>
+                <a className="nav-link active" aria-current="page" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
+</svg>
+                    Home</a>
+                <a className="nav-link" href="#">
+                <img className='avatar' src={myAvatar} alt="avatar" />
+                    Profile</a>
             </nav>  
         </body>
     )
