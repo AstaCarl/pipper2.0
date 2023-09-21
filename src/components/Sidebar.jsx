a164239100ed89eac4fc9db2fbb7629e130112c3
Asta Carl <astamig@hotmail.com>
2023-09-21 12:57:35 +0200

    button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import myLogo from './twitter.logo.png'
 import myAvatar from './adventurer-1695289491040.png'
+import "./knap.css"
+import Knap from "./Knap"
 
 export default function Sidebar(props) {
     return (
@@ -14,12 +16,13 @@ export default function Sidebar(props) {
                 </figure>
                 <a className="nav-link active" aria-current="page" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
-  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
-</svg>
-                    Home</a>
+                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
+                </svg>Home
+                </a>
                 <a className="nav-link" href="#">
-                <img className='avatar' src={myAvatar} alt="avatar" />
-                    Profile</a>
+                <img className='profil-avatar' src={myAvatar} alt="avatar" />Profile
+                </a>
+                < Knap/>
             </nav>  
         </body>
     )
---
+++ src/components/knap.jsx
@@ -0,0 +1,10 @@
+
+export default function Knap(props) {
+    return(
+        <div>
+            <button className="myBtn">
+                Opret Pip
+            </button>
+        </div>
+    )
+}
