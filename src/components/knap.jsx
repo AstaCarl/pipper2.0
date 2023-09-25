0d8ca5facf9836fdb6c8ab499fbf42a7bc05c328
AstaCarl <astamig@hotmail.comm>
2023-09-25 11:59:25 +0200

    deletebutton function

--- src/components/knap.jsx
+++ src/components/knap.jsx
@@ -1,15 +1,8 @@
 export default function Knap(props) {
-        const handleButtonClick = () => {
-          const pips = document.getElementById("pips");
-          if (pips.style.display === 'none') {
-            pips.style.display = 'block';
-          } else {
-            pips.style.display = 'none';
-          }
-        };
+
     return(
         <div>
-            <button className="myBtn" id="myBtn" onClick={handleButtonClick}>
+            <button className="myBtn" id="myBtn">
                 Opret Pip
             </button>
         </div>
