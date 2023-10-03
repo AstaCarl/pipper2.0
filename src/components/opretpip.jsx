9eb062c2b255e37662d9775ab69f6694dac93795
Julie <jc.lauesen@gmail.com>
2023-10-03 19:30:16 +0200

    fjernet den som jeg var startet på

--- src/components/opretpip.jsx
+++ src/components/opretpip.jsx
@@ -13,13 +13,7 @@ export default function Opretpip() {
     const [username, setUsername] = useState('');
     const [content, setContent] = useState('');
     const [pips, setPips] = useState([]);
-
-    // //Jeg er i gang med at prøve at implementerer, at man ikke kan opret pip uden at have udfyldt det hele og dermed kommer der en besked op. 
-    // const [usernameTodo, setusernameTouchedTodo] = useState(false);
-    // const [contentTodo, setcontentTouchedTodo] = useState(false);
-    // const [pipsTodo, setpipsTouchedTodo] = useState(false);
-
-
+    
     const handleUsernameChange = (e) => {
         console.log(e.target.value);
         //Fanger det bruger navn som brugeren skriver ind i input
